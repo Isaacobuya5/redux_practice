6ca95cae7b3e886d816b4318140ece1a1bd4b522
Isaac Obuya <Szack4477@gmail.com>
2019-12-13 18:39:27 +0300

    create header, courses and header page and implement routing

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CoursePage extends React.Component {
+  render() {
+    return <h2>Course Page Here</h2>;
+  }
+}
+
+export default CoursePage;
---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// enables babel to know that we are running in development mode
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map", //give us a source map for debugging i.e. see original code
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    // serving our app, we can use any node server including express too
+    stats: "minimal", //reduces information it writes to command line
+    overlay: true, // overlay any error that occur to the browser
+    historyApiFallback: true, // all requests will be sent to index.html
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"] // babel transpiles all our code and then webpack will handle it for us
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
